480d9b3176f05ecb879b115572e15b60198b9c11
wave1992 <wangxin26@58ganji.com>
2016-05-25 15:54:33 +0800

    上下滑动图片区域，body可正常touchmove。

--- slide/t2.js
+++ slide/t2.js
@@ -15,10 +15,13 @@ TCLife.prototype = {
 	* @method bindSwipe
 	* 电影海报区域滑动事件
 	*/
-	bindSwipe:function(){
+	bindSwipe:function(outer){
 		var self = this;
 		var scaleW = self.scaleW;
-		var outer = self.outer;
+		// var outer = self.outer;
+		if(!outer){
+			return;
+		}
 		//ul 与 屏幕宽度之间的 差值
 		var initDistance =  scaleW - outer.offsetWidth ;
 
@@ -33,43 +36,53 @@ TCLife.prototype = {
 		//边界值
 		var bound = {
 			left:0 ,//左边界值
-			right:initDistance>0?0:initDistance,  //右边界值
-			count:0 //几个手指在屏幕上start
+			right:initDistance>0?0:initDistance  //右边界值
 		} ;
 
 		var TAG = {
 			startTime:0 , //touchstart时的起始时间 && touchmove时，上一段 move的结束时间
 			endTime:0 ,  //touchmove时，本段 move的结束时间
-			speed:0   //速度，touchmove时，每段move的 距离除以 每段move的时间
+			speed:0,   //速度，touchmove时，每段move的 距离除以 每段move的时间
+			count:0
 		} ;
 
 		//手指按下的处理事件
 		var startHandler = function(e){
 			$(outer).removeClass('swiper-trans') ;
 
-			pos.lastX = pos.currentX = e.touches[0].pageX ;
+			TAG.count++;
+
+			pos.lastX = pos.currentX = pos.initX = e.touches[0].pageX ;
 			pos.perDiffX = 0 ;//每段move的 手指偏移量 offsetX;start时要清空
 
 			pos.transX = outer.getBoundingClientRect().left ; //transform 的 translateX值
 
 			TAG.startTime = new Date() * 1 ;
-			// 几个手指   count就是几
-			TAG.count ++ ;
-		};
+			
+			pos.lastY = pos.currentY = pos.initY = e.touches[0].pageY; 
 
+		};
+		//上下滑动还是 左右滑动
+		var swipeDirection = function(x1, x2, y1, y2){
+		    var xDis = Math.abs(x1 - x2), yDis = Math.abs(y1 - y2)
+	        return xDis >= yDis ? (x1 - x2 > 0 ? 'left' : 'right') : (y1 - y2 > 0 ? 'up' : 'down')
+	  	};
 		//手指移动的处理事件
 		var moveHandler = function(e){
-			//兼容chrome android，阻止浏览器默认行为
-			e.preventDefault();
-			//计算手指的偏移量
 
+			//计算手指X方向的偏移量
+			pos.currentY = e.touches[0].pageY ;
+			
 			pos.currentX = e.touches[0].pageX ;
 
 			pos.perDiffX = pos.currentX - pos.lastX ;
 
 			pos.lastX = pos.currentX ;
 
-			
+			var str = swipeDirection(pos.initX,pos.currentX,pos.initY,pos.currentY);
+			if(str == 'left' || str == 'right'){
+				e.preventDefault();
+			}
 
 			// touchmove 超过边界，滑动距离 按比率减少，变成弹力效果
 			if(pos.transX>=bound.left){
@@ -83,7 +96,12 @@ TCLife.prototype = {
 
 			pos.transX += pos.perDiffX ;
 
-			self.outer.style.webkitTransform = 'translateX('+ pos.transX +'px)';
+			$(outer).removeClass('swiper-trans') ;
+
+			// self.outer.style.transform = 'translate('+ pos.transX +'px,0)';
+			// self.outer.style.webkitTransform = 'translate('+ pos.transX +'px,0)';
+			outer.style.transform = 'translate('+ pos.transX +'px,0)';
+			outer.style.webkitTransform = 'translate('+ pos.transX +'px,0)';
 
 			// 计算速度
 			TAG.endTime = new Date()*1 ;
@@ -94,14 +112,14 @@ TCLife.prototype = {
 		//手指抬起的处理事件
 		var endHandler = function(e){
 			e.preventDefault();
-			$(outer).addClass('swiper-trans') ;
-			// 几个手指   count就是几
+
 			TAG.count--;
 
-			//count > 0 证明还有 手指 在上面，后续 不执行
-			if(TAG.count > 0){
+			if(TAG.count>0){
 				return;
 			}
+
+			$(outer).addClass('swiper-trans') ;
 
 			if(Math.abs(TAG.speed)>40){
 				pos.transX += 0.168*TAG.speed ;
@@ -115,15 +133,17 @@ TCLife.prototype = {
 			if(pos.transX<=bound.right){
 				pos.transX = bound.right ;
 			}
+			outer.style.transform = 'translate('+pos.transX+'px,0)' ;
+			outer.style.webkitTransform = 'translate('+pos.transX+'px,0)' ;
 
-			outer.style.transform = 'translateX('+pos.transX+'px)' ;
-			
 		};
+
+
 		outer.addEventListener('touchstart', startHandler,false);
 		outer.addEventListener('touchmove', moveHandler,false);
 		outer.addEventListener('touchend', endHandler,false);
 		outer.addEventListener('touchcancel', endHandler,false);
-	},
+	}
 
 
 
